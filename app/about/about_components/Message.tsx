ba258c226047a781975e8f2eb6c5dddfdf02e41d
Nugar <cladeadenugar@gmail.com>
2025-01-03 18:57:10 +0100

    prod fix

--- app/about/about_components/Message.tsx
+++ app/about/about_components/Message.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import { dmSerifFont, jostFont } from "@/app/util/font";
 
 const Message = () => {
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
   return (
     <section className="flex flex-col space-y-12 md:space-y-16 mb-10">
       <div className="flex justify-center items-center">
@@ -10,7 +15,7 @@ const Message = () => {
           Creative project? Let&apos;s have a productive talk.
         </h1>
       </div>
-      <form className={`${jostFont.className}`}>
+      <form className={`${jostFont.className}`} onSubmit={handleSubmit}>
         <div className="sm:flex space-y-16 sm:space-y-0 sm:space-x-10">
           <input
             type="text"
@@ -35,7 +40,7 @@ const Message = () => {
           className="border-[#000000] border-b md:text-[1.125rem] pb-1 lg:text-[1.25rem] text-[#4D5053] mt-16 placeholder:text-[#4D5053] align-bottom pt w-full focus:outline-none"
         />
         <div className="flex justify-center items-center mt-8">
-          <button className="bg-[#292F36] text-[0.875rem] lg:text-[1.125rem] text-white shadow-md rounded-[1.125rem] py-3 px-5">
+          <button className="bg-[#292F36] hover:bg-[#000000] text-[0.875rem] lg:text-[1.125rem] text-white shadow-md rounded-[1.125rem] py-3 px-5">
             Send Now
           </button>
         </div>
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,14 +5,14 @@ const NotFound = () => {
     <section className="mt-16 mb-10 xl:px-28 px-5 lg:px-16 2xl:flex 2xl:justify-center min-h-screen 2xl:items-center sm:px-10">
       <div className={`${dmSerifFont.className}`}>
         <h1
-          className={`text-[#CDA274] text-[5rem] md:text-[9.375rem] lg:text-[12.5rem]`}
+          className={`text-[#CDA274] text-[6.25rem] md:text-[9.375rem] lg:text-[12.5rem]`}
         >
           404
         </h1>
         <h2 className="text-[#292F36] max-w-[569px] text-[1.125rem] md:text-[1.875rem] lg::text-[2.188rem]">
-          We are sorry, but the page you requested for was not found
+          We are sorry, but the page you requested for was not found!
         </h2>
-        <button className="bg-[#292F36] text-[0.875rem] mt-10 lg:text-[1.125rem] text-white shadow-md rounded-[1.125rem] py-3 px-5">
+        <button className="bg-[#292F36] text-[0.875rem] mt-10 lg:text-[1.125rem] text-white shadow-md rounded-[0.5rem] py-3 px-5">
           Back To Home
         </button>
       </div>
