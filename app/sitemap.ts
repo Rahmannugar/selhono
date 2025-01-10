383dec90a63c11a9ee30a1274decc9ea4c1ff6b9
Nugar <cladeadenugar@gmail.com>
2025-01-10 13:52:39 +0100

    updated sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,43 +3,43 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://selhono.vercel.app",
+      url: "https://selhonointeriors.vercel.app",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://selhono.vercel.app/services",
+      url: "https://selhonointeriors.vercel.app/services",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.8,
     },
     {
-      url: "https://selhono.vercel.app/team",
+      url: "https://selhonointeriors.vercel.app/team",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.6,
     },
     {
-      url: "https://selhono.vercel.app/contact",
+      url: "https://selhonointeriors.vercel.app/contact",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.6,
     },
     {
-      url: "https://selhono.vercel.app/project",
+      url: "https://selhonointeriors.vercel.app/project",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://selhono.vercel.app/about",
+      url: "https://selhonointeriors.vercel.app/about",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.6,
     },
     {
-      url: "https://selhono.vercel.app/blog",
+      url: "https://selhonointeriors.vercel.app/blog",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
